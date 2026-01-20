d5f6d1df71a2f731ac02370a6fb708cd71c87b6c
Sahafkop9696 <skopbeats@gmail.com>
2026-01-20 14:28:15 +0530

    simple calculator with double datatype input

---
+++ A08_DoubleSimpleCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double i, j, result = 0;
+
+Console.WriteLine("Value 1 :");
+i = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Value 2 :");
+j = Double.Parse(Console.ReadLine());
+
+result = i+ j;
+Console.WriteLine(result);
