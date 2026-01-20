503b10ffb492d39e346da264b9c932dd522a2ea5
Sahafkop9696 <skopbeats@gmail.com>
2026-01-20 14:11:04 +0530

    Multime Memory Variable Assignments

---
+++ 2026_01_20_Hello/HelloConsoleVSC/A03_ReadLine/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+string name;
+string location;
+Console.Write("Name : ");
+name = Console.ReadLine();
+
+Console.Write("Location : ");
+location = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}, located at {location}");
+
---
+++ A06_IntMemoryVariable/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int i, j, result;
+
+i = 50;
+j = 60;
+result = i + j;
+
+Console.WriteLine(result);
