dd7410bdfe4f4448840a6a688dd824bec478c120
Sahafkop9696 <skopbeats@gmail.com>
2026-01-20 14:20:29 +0530

    Simple Int Calculator

---
+++ A07_IntSimpleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int i, j, result = 0;
+
+Console.WriteLine("Value 1 :");
+i = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Value 2 :");
+j = Int32.Parse(Console.ReadLine());
+
+result = i + j;
+
+Console.WriteLine(result);
