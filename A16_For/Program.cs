9d298fe4736fd741c0acc3b24aabc4e7e72d005a
Sahafkop9696 <skopbeats@gmail.com>
2026-01-21 00:37:24 +0530

    If statement assignment

---
+++ A16_For/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Hello {i}");
+}
+
+Console.WriteLine("Less or Equal to" );
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"Hello {i}");
+}
+
+Console.WriteLine("Odd numbers" );
+
+for (int i =10; i < 20; i = i+2)
+{
+    Console.WriteLine($"Hello {i}");
+}
